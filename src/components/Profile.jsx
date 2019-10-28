34cbe61f84bc7e9dfe2b5bf1c4480af109da190b
annabalab <anna.balabanskaya@mail.ru>
2019-10-28 17:21:23 +0300

    css-modules, add sass

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import s from './Profile.module.scss'
 
 const Profile = () => {
   return (
-      <div className='content'>
+      <div className={s.content}>
         <div>
           <img
               src={'https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'}/>
