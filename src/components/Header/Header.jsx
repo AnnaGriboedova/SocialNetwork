ba536d3cecad29ca6ace320b15ee398b86a5bbaa
Anna Griboedova <ann.griboedova@gmail.com>
2020-04-18 21:05:15 +0300

    added authReducer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import s from './Header.module.scss'
 import logo from './logo1.png';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
       <header className={s.header}>
         <img src={logo} alt='scanface logo'/>
+        <div className={s.loginBlock}>
+            {props.isAuth?props.login
+            :<NavLink to={'/login'}>Login</NavLink>
+            }
+        </div>
       </header>
   )
 };
