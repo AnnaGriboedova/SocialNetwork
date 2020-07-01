e923272a13470842ee3ffd2f35879d6677c86f46
Anna Griboedova <ann.griboedova@gmail.com>
2020-07-01 13:54:26 +0300

    used setState, local state

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -33,5 +33,5 @@ export default compose(
         getUserProfile
     }),
     withRouter,
-    withAuthRedirect
+    //withAuthRedirect
 )(ProfileContainer);
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from './ProfileInfo.module.scss'
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
     if (!props.userProfile) {
@@ -8,14 +9,14 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div>
+            {/*<div>
                 <img className={s.image}
                      src={'https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'}/>
-            </div>
+            </div>*/}
             <div>
                 <img className={s.image}
                      src={props.userProfile.photos.large}/>
-                Ava+desc
+                <ProfileStatus status='Helloooo'/>
             </div>
         </div>
     )
