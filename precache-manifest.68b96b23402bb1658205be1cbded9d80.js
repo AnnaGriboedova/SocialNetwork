1e29dcfd74236980e2eb7e275d8f46acfeacba6f
Anna Griboedova <ann.griboedova@gmail.com>
2020-12-22 00:59:43 +0300

    Updates

---
+++ precache-manifest.68b96b23402bb1658205be1cbded9d80.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fe3d58679cafb57023e496bb9f9dfb",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "d48c4c6a5f862216621a",
+    "url": "/SocialNetwork/static/css/3.d1d9a9c7.chunk.css"
+  },
+  {
+    "revision": "cc60735b76626666f1a0",
+    "url": "/SocialNetwork/static/css/4.aa1a1108.chunk.css"
+  },
+  {
+    "revision": "fbc2ba87950e9f49bad4",
+    "url": "/SocialNetwork/static/css/main.f603470e.chunk.css"
+  },
+  {
+    "revision": "954c5b4b362c350c9238",
+    "url": "/SocialNetwork/static/js/2.58598727.chunk.js"
+  },
+  {
+    "revision": "291e0d2bc65cd0d19fff9204b8020a70",
+    "url": "/SocialNetwork/static/js/2.58598727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48c4c6a5f862216621a",
+    "url": "/SocialNetwork/static/js/3.27a35407.chunk.js"
+  },
+  {
+    "revision": "cc60735b76626666f1a0",
+    "url": "/SocialNetwork/static/js/4.d253a07a.chunk.js"
+  },
+  {
+    "revision": "0b6a39cde822b217e3b8",
+    "url": "/SocialNetwork/static/js/5.926f8647.chunk.js"
+  },
+  {
+    "revision": "fbc2ba87950e9f49bad4",
+    "url": "/SocialNetwork/static/js/main.80e65bfb.chunk.js"
+  },
+  {
+    "revision": "44f5231e35fcda60f567",
+    "url": "/SocialNetwork/static/js/runtime-main.fafe6082.js"
+  },
+  {
+    "revision": "84d76513e00664058c76166c7c95882f",
+    "url": "/SocialNetwork/static/media/AXIS-ExtraBold.84d76513.eot"
+  },
+  {
+    "revision": "9f33d28d1c15d63363c308bbc9ad3659",
+    "url": "/SocialNetwork/static/media/AXIS-ExtraBold.9f33d28d.woff"
+  },
+  {
+    "revision": "a53e363d64a2d6c703719a800f042486",
+    "url": "/SocialNetwork/static/media/AXIS-ExtraBold.a53e363d.ttf"
+  },
+  {
+    "revision": "cefeb0f778d3f056f8ea06bdaf977661",
+    "url": "/SocialNetwork/static/media/AXIS-ExtraBold.cefeb0f7.svg"
+  },
+  {
+    "revision": "e5dfe039cf21781e28e72755bfd911fb",
+    "url": "/SocialNetwork/static/media/AXIS-ExtraBold.e5dfe039.woff2"
+  },
+  {
+    "revision": "6a38616c1c6a202575ff607b34553801",
+    "url": "/SocialNetwork/static/media/ball.6a38616c.svg"
+  },
+  {
+    "revision": "eca3e50e63870953d3fd2b9c008af8e8",
+    "url": "/SocialNetwork/static/media/chat.eca3e50e.svg"
+  },
+  {
+    "revision": "794e6356407f4be4fccee6af42acf7fb",
+    "url": "/SocialNetwork/static/media/cupcake.794e6356.svg"
+  },
+  {
+    "revision": "24cb95238b6b7c84787691133ee1972c",
+    "url": "/SocialNetwork/static/media/earth.24cb9523.svg"
+  },
+  {
+    "revision": "e720936c82abd8df2e2a350f1e8c35a1",
+    "url": "/SocialNetwork/static/media/edit.e720936c.svg"
+  },
+  {
+    "revision": "dee886cea7fd0324bff415389788ff6e",
+    "url": "/SocialNetwork/static/media/facebook.dee886ce.svg"
+  },
+  {
+    "revision": "b938b69d2717025937f34834d2aa1dd3",
+    "url": "/SocialNetwork/static/media/faq.b938b69d.svg"
+  },
+  {
+    "revision": "844006a197f5b421e1ab0946166a1f59",
+    "url": "/SocialNetwork/static/media/flag.844006a1.svg"
+  },
+  {
+    "revision": "0dd82fda1bbf4b773c4aef3ef40d96f2",
+    "url": "/SocialNetwork/static/media/footprint.0dd82fda.svg"
+  },
+  {
+    "revision": "7dea87fd4598d6a2047bed4260d0cccf",
+    "url": "/SocialNetwork/static/media/github.7dea87fd.svg"
+  },
+  {
+    "revision": "d6b4fe7b37dd0366730e92540a3b1907",
+    "url": "/SocialNetwork/static/media/group-profile-users.d6b4fe7b.svg"
+  },
+  {
+    "revision": "7addff74e00d1d02ba7c67559a20eda4",
+    "url": "/SocialNetwork/static/media/instagram.7addff74.svg"
+  },
+  {
+    "revision": "d411fef8d19da5419aa29e174c93eb8b",
+    "url": "/SocialNetwork/static/media/like.d411fef8.svg"
+  },
+  {
+    "revision": "280f196ba25d39058705167445ca8108",
+    "url": "/SocialNetwork/static/media/loading.280f196b.svg"
+  },
+  {
+    "revision": "b09d287628fca32097531ac007274c51",
+    "url": "/SocialNetwork/static/media/logo1.b09d2876.png"
+  },
+  {
+    "revision": "4696fa93dd91adf8ae7e18b8ec130420",
+    "url": "/SocialNetwork/static/media/logout.4696fa93.svg"
+  },
+  {
+    "revision": "2822fb052ddf71428b58c0beecc5d320",
+    "url": "/SocialNetwork/static/media/more.2822fb05.svg"
+  },
+  {
+    "revision": "d78cdd7cddd4bd60edcbe96cfc16a74b",
+    "url": "/SocialNetwork/static/media/music.d78cdd7c.svg"
+  },
+  {
+    "revision": "2cc32a0a5a8f4cee553aec49234e5ece",
+    "url": "/SocialNetwork/static/media/newspaper.2cc32a0a.svg"
+  },
+  {
+    "revision": "4c03f7273c307c2ec010d0c156896d71",
+    "url": "/SocialNetwork/static/media/profile.4c03f727.png"
+  },
+  {
+    "revision": "cf543b7a29f8b091382284122c510f87",
+    "url": "/SocialNetwork/static/media/profile.cf543b7a.svg"
+  },
+  {
+    "revision": "4e80bd4bb7632d61240a6f2a389f3d8c",
+    "url": "/SocialNetwork/static/media/settings.4e80bd4b.svg"
+  },
+  {
+    "revision": "88ab01807701b234e8d5b5e8aed9b0b0",
+    "url": "/SocialNetwork/static/media/smile.88ab0180.svg"
+  },
+  {
+    "revision": "58ec9f2688e5d8bf1d5e419706e015df",
+    "url": "/SocialNetwork/static/media/smile1.58ec9f26.svg"
+  },
+  {
+    "revision": "f7bd3ca4e307c585409da3cc5b671ff5",
+    "url": "/SocialNetwork/static/media/t-shirt.f7bd3ca4.svg"
+  },
+  {
+    "revision": "2633d7326d6cf65db6fe1bb3cbbd5438",
+    "url": "/SocialNetwork/static/media/thinking.2633d732.svg"
+  },
+  {
+    "revision": "eeda54ff51850f731a13d9f595ba0a26",
+    "url": "/SocialNetwork/static/media/twitter.eeda54ff.svg"
+  },
+  {
+    "revision": "f7c8a4b95286ae5402fc2f2883fef2a2",
+    "url": "/SocialNetwork/static/media/vk.f7c8a4b9.svg"
+  },
+  {
+    "revision": "2265521195efdb959dfaede294aa4454",
+    "url": "/SocialNetwork/static/media/youtube.22655211.svg"
+  }
+]);
---
+++ static/js/5.926f8647.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-1"]=this["webpackJsonpreact-1"]||[]).push([[5],{332:function(t,n,c){"use strict";function r(t,n){return t+n}c.r(n),c.d(n,"add",(function(){return r}))}}]);
+//# sourceMappingURL=5.926f8647.chunk.js.map
